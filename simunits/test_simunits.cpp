cbccf2b205850af9503261713af86f12cc623feb
Bernhard Thiele <bernhard.thiele@liu.se>
2017-02-28 16:36:33 +0100

    Using private vectors with getters/setters

--- simunits/test_simunits.cpp
+++ simunits/test_simunits.cpp
@@ -16,13 +16,20 @@ int main() {
 
   std::cout << std::endl << "Creating ports ..." << std::endl;
   core::Port port1;
-  simUnit.push_back(port1);
   core::Port port2("MyPortName");
-  simUnit.push_back(port2);
+  std::vector<core::Port> myports;
+  myports.push_back(port1);
+  myports.push_back(port2);
+  simUnit.ports(myports);
 
   std::cout << "Iterating over ports of SimUnit: " << std::endl;
-  core::SimUnit::iterator iter;
-  for (iter = simUnit.begin(); iter != simUnit.end(); iter++) {
+  std::vector<core::Port>::iterator iter;
+  // NOT WORKING!?
+  // for (iter = myports.begin(); iter != myports.end(); iter++) {
+  //   std::cout << iter->name() << std::endl;
+  // }
+  std::vector<core::Port> myports2  = simUnit.ports();
+  for (iter = myports2.begin(); iter != myports2.end(); iter++) {
     std::cout << iter->name() << std::endl;
   }
 
@@ -31,7 +38,9 @@ int main() {
   std::cout << "Instantiated CompositeSimUnit with name: " << compositeSimUnit.name() << std::endl;
 
   std::cout << std::endl << "Adding SimUnit to CompositeSimUnit ..." << std::endl;
-  compositeSimUnit.simunit_push_back(&simUnit);
+  std::vector<core::SimUnit*> mySimUnits;
+  mySimUnits.push_back(&simUnit);
+  compositeSimUnit.simUnits(mySimUnits);
 
   std::cout << std::endl << "Creating Connector ..." << std::endl;
   core::Connector connector("myConnect");
@@ -40,19 +49,23 @@ int main() {
   connector.port_a(&port1);
   connector.port_b(&port2);
   std::cout << "Adding Connector to CompositeSimUnit ..." << std::endl;
-  compositeSimUnit.connector_push_back(connector);
+  std::vector<core::Connector> myConnectors;
+  compositeSimUnit.connectors(myConnectors);
 
   std::cout << std::endl << "Traversing the CompositeSimUnit ..." << std::endl;
-  core::CompositeSimUnit::simunit_iterator iter1;
-  for (iter1 = compositeSimUnit.simunit_begin(); iter1 != compositeSimUnit.simunit_end(); iter1++) {
+  std::vector<core::SimUnit*>::iterator iter1;
+  std::vector<core::SimUnit*> mySimUnits2 = compositeSimUnit.simUnits();
+  for (iter1 = mySimUnits2.begin(); iter1 != mySimUnits2.end(); iter1++) {
     std::cout << "Simunit: " << (*iter1)->name() << std::endl << "\twith ports: " << std::endl;
-    core::SimUnit::iterator iter2;
-    for (iter2 = (*iter1)->begin(); iter2 != (*iter1)->end(); iter2++) {
+    std::vector<core::Port> myports3 = (*iter1)->ports();
+    std::vector<core::Port>::iterator iter2;
+    for (iter2 = myports3.begin(); iter2 != myports3.end(); iter2++) {
       std::cout << "\t" << iter2->name() << std::endl;
     }
   }
-  core::CompositeSimUnit::connector_iterator iter3;
-  for (iter3 = compositeSimUnit.connector_begin(); iter3 != compositeSimUnit.connector_end(); iter3++) {
+  std::vector<core::Connector>::iterator iter3;
+  std::vector<core::Connector> myConnectors2 = compositeSimUnit.connectors();
+  for (iter3 = myConnectors2.begin(); iter3 != myConnectors2.end(); iter3++) {
     std::cout << "Connector: " << iter3->name() << "(" << iter3->port_a()->name() <<
       ", " << iter3->port_b()->name() <<  ")" << std::endl;
   }
