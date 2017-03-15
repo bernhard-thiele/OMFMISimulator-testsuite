67935e36372eb1b4d0a915ddfedb35c85ac2a2d3
Bernhard Thiele <bernhard.thiele@liu.se>
2017-03-15 01:14:47 +0100

    Design revision using abstract interfaces in simunits::core

--- simunits/test_simunits_fmi20.cpp
+++ simunits/test_simunits_fmi20.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 #include "src/simunits/fmi20/FMU20.h"
-#include "src/simunits/core/Port.h"
-#include "src/simunits/core/CompositeSimUnit.h"
-#include "src/simunits/core/Connector.h"
+
+#include "src/simunits/fmi20/OutputRealPort.h"
+// #include "src/simunits/core/CompositeSimUnit.h"
+// #include "src/simunits/core/Connector.h"
 
 using namespace simunits;
 
@@ -13,9 +14,14 @@ int main() {
   //std::string modelpath = "/home/bernhard/data/workspace/OMSimulator/testsuite/FMUs/cs_PID_Controller.fmu";
   std::string modelpath = "/home/bernhard/data/workspace/OMSimulator/testsuite/FMUs/me_BouncingBall.fmu";
 
+  fmi20::OutputRealPort outRp("Testme");
+  std::cout << "Instantiated OutputRealPort name: " << outRp.name() << std::endl;
+  std::cout << "Instantiated OutputRealPort os: " << outRp << std::endl;
+
+
   std::cout << "Creating simunit ..." << std::endl;
   fmi20::FMU20 simUnit(modelpath);
-  std::cout << "Instantiated SimUnit name: " << simUnit.name << std::endl;
+  std::cout << "Instantiated SimUnit name: " << simUnit.name() << std::endl;
 
-  simUnit.simulate();
+  //simUnit.simulate();
 }
