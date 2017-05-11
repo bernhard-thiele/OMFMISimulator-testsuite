46b9928d92610e476e68c41abe000c949170b49d
Lennart Ochel <lennart.ochel@liu.se>
2017-05-11 15:17:01 +0200

    Activate test2.lua

--- OMSimulatorLua/test2.lua
+++ OMSimulatorLua/test2.lua
@@ -14,11 +14,14 @@ setWorkingDirectory(model, ".")
 -- instantiate FMUs
 instantiateFMU(model, "../FMUs/me_source1.fmu", "sourceA")
 instantiateFMU(model, "../FMUs/me_source1.fmu", "sourceB")
-instantiateFMU(model, "../FMUs/me_adder1.fmu", "adder")
+instantiateFMU(model, "../FMUs/me_adder1.fmu", "adder1")
+instantiateFMU(model, "../FMUs/me_adder1.fmu", "adder2")
 
 -- add connections
-addConnection(model, "sourceA.y", "adder.x1")
-addConnection(model, "sourceB.y", "adder.x2")
+addConnection(model, "sourceA.y", "adder1.x1")
+addConnection(model, "sourceB.y", "adder1.x2")
+addConnection(model, "adder1.y", "adder2.x1")
+addConnection(model, "sourceB.y", "adder2.x2")
 
 -- set parameter
 setReal(model, "sourceA.A", 0.5)
@@ -26,11 +29,19 @@ setReal(model, "sourceA.omega", 2.0)
 
 setStopTime(model, 10.0)
 
-simulate(model)
+initialize(model)
+stepUntil(model, 10.0)
 --exportDependencyGraph(model, "test")
---describe(model)
+--os.execute("dot -Tpdf test_outputsGraph.dot > test_outputsGraph.pdf")
+
+print("adder1.y at 10: " .. getReal(model, "adder1.y"))
+print("adder2.y at 10: " .. getReal(model, "adder2.y"))
+
+terminate(model)
 
 unload(model)
 
 -- Result:
+-- adder1.y at 10: -0.050699025021762
+-- adder2.y at 10: -0.48303321299316
 -- endResult
