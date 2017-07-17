f02b54f505f820013da3b29916c13877e59a5947
Bernhard Thiele <bernhard.thiele@liu.se>
2017-07-17 18:03:24 +0200

    Support for testing with CERN ROOT framework
    Ported the Lua example for the bouncing ball FMU
    so that it can be executed within the
    CERN ROOT Data Analysis Framework.

---
+++ OMSimulatorRoot/test_cs_BouncingBall.C
@@ -0,0 +1,57 @@
+// name: test_cs_BouncingBall
+// status: correct
+
+/** Before the script runs in root, one needs to load the OMSimulator shared library on the REPL
+ *
+ * gSystem->Load("../../3rdParty/FMIL/install/linux/lib/libfmilib_shared.so");
+ * gSystem->Load("../../install/lib/libOMSimulatorLib.so");
+ */
+
+#include "../../install/include/OMSimulator.h"
+
+void test_cs_BouncingBall() {
+  const char* version = oms_getVersion();
+  std::cout << version << std::endl;
+
+  void* model = oms_newModel();
+  oms_setTempDirectory(".");
+
+  oms_instantiateFMU(model, "../FMUs/cs_BouncingBall.fmu", "BouncingBall");
+  oms_describe(model);
+
+  oms_setStopTime(model, 2.1);
+  oms_setTolerance(model, 1e-5);
+
+  oms_simulate(model);
+
+  oms_unload(model);
+}
+
+// Result:
+//
+// Processing test_cs_BouncingBall.C...
+// OMSimulator v0.0.0-129-gb89431c
+// # FMU instances
+// BouncingBall
+//   - FMI 2.0 CS
+//   - path: ../FMUs/cs_BouncingBall.fmu
+//   - GUID: {ed0c3e8c-c48f-4995-b80b-eb1d54c9737b}
+//   - tool: OpenModelica Compiler OMCompiler v1.12.0-dev.395+gdeeabde
+//   - input interface:
+//   - output interface:
+//
+// # Simulation settings
+//   - start time: 0
+//   - stop time: 0
+//   - tolerance: 0
+//   - result file: <no result file>
+//
+// # Composite structure
+// ## Initialization
+//
+// ## Simulation
+//
+// Logging information has been saved to "omsllog.txt"
+//   25 warnings
+//   0 errors
+// endResult
